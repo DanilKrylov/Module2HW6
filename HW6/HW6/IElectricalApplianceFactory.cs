38c2e7d0b83310f36e1a67822154f128e230b8c2
Krylov Danil <danuha2350@gmail.com>
2022-02-15 17:19:32 +0200

    Revert "add HW6"
    This reverts commit 09246cf7f2f9cea5d03a77614ae257265d41752c.

--- HW6/HW6/IElectricalApplianceFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HW6
-{
-    public interface IElectricalApplianceFactory
-    {
-        public ElectricalAppliance CreateElectrialAppliance(string name, int energyCost);
-
-        public IWire CreateWire();
-    }
-}
